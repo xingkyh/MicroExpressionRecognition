810d5fbde45d4d7c11350e4d34c36dcd4cd8f54c
xingkyh <1726284922@qq.com>
2021-05-24 17:46:01 +0800

    项目起始

---
+++ model.py
@@ -0,0 +1,38 @@
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import PReLU
+
+
+def CNN(input_shape=(48, 48, 1), n_classes=8):
+    """
+    参考论文实现
+    A Compact Deep Learning Model for Robust Facial Expression Recognition
+    :param input_shape:
+    :param n_classes:
+    :return:
+    """
+    # input
+    input_layer = Input(shape=input_shape)
+    x = Conv2D(32, (1, 1), strides=1, padding='same', activation='relu')(input_layer)
+    # block1
+    x = Conv2D(64, (3, 3), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = Conv2D(64, (5, 5), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # block2
+    x = Conv2D(64, (3, 3), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = Conv2D(64, (5, 5), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # fc
+    x = Flatten()(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(n_classes, activation='softmax')(x)
+
+    model = Model(inputs=input_layer, outputs=x)
+    return model
---
+++ visualize.py
@@ -0,0 +1,62 @@
+"""
+desc: 可视化训练过程
+"""
+
+
+def load_file(filename):
+    import pickle
+    data_file = open(filename, 'rb')
+    data = pickle.load(data_file)
+    data_file.close()
+    return data.history
+
+
+def plot_loss(his, ds):
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(12, 8))
+    plt.plot(np.arange(len(his['loss'])), his['loss'], label='train loss')
+    plt.plot(np.arange(len(his['val_loss'])), his['val_loss'], label='valid loss')
+    plt.title(ds + ' training loss')
+    plt.legend(loc='best')
+    plt.savefig('./assets/his_loss.png')
+
+
+def plot_acc(his, ds):
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(12, 8))
+    plt.plot(np.arange(len(his['accuracy'])), his['accuracy'], label='train accuracy')
+    plt.plot(np.arange(len(his['val_accuracy'])), his['val_accuracy'], label='valid accuracy')
+    plt.title(ds + ' training accuracy')
+    plt.legend(loc='best')
+    plt.savefig('./assets/his_acc.png')
+
+
+def plot_feature_map():
+    from model import CNN
+    model = CNN()
+    model.load_weights('../models/cnn3_best_weights.h5')
+
+    def get_feature_map(model, layer_index, channels, input_img):
+        from tensorflow.keras import backend as K
+        layer = K.function([model.layers[0].input], [model.layers[layer_index].output])
+        feature_map = layer([input_img])[0]
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(20, 8))
+        for i in range(channels):
+            img = feature_map[:, :, :, i]
+            plt.subplot(4, 8, i + 1)
+            plt.imshow(img[0], cmap='gray')
+        plt.savefig('rst.png')
+        plt.show()
+        import cv2
+        img = cv2.cvtColor(cv2.imread('../data/demo.jpg'), cv2.cv2.COLOR_BGR2GRAY)
+        img.shape = (1, 48, 48, 1)
+        get_feature_map(model, 4, 32, img)
+
+
+if __name__ == '__main__':
+    import numpy as np
+    history = load_file('../train_results/his_cnn2.pkl')
+    print(np.max(history['val_acc']))
+    plot_loss(history, "fer")
+    plot_acc(history, "fer")
