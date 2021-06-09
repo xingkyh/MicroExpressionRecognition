831dca3f5029561385188f57033f01c237849e5a
xingkyh <1726284922@qq.com>
2021-06-09 20:23:35 +0800

    增加用于对比的其它模型

--- model.py
+++ model.py
@@ -36,3 +36,69 @@ def CNN(input_shape=(48, 48, 1), n_classes=8):
 
     model = Model(inputs=input_layer, outputs=x)
     return model
+
+
+def CNN1(input_shape=(48, 48, 1), n_classes=8):
+    """
+    参考VGG思路设计的模型，主要注意点是感受野不能太大，以免获得很多噪声信息
+    :param input_shape: 输入图片的尺寸
+    :param n_classes: 目标类别数目
+    :return:
+    """
+    # input
+    input_layer = Input(shape=input_shape)
+    # block1
+    x = Conv2D(32, kernel_size=(3, 3), strides=1, padding='same', activation='relu')(input_layer)
+    x = Conv2D(32, kernel_size=(3, 3), strides=1, padding='same', activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = Dropout(0.5)(x)
+    # block2
+    x = Conv2D(64, kernel_size=(3, 3), strides=1, padding='same', activation='relu')(x)
+    x = Conv2D(64, kernel_size=(3, 3), strides=1, padding='same', activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = Dropout(0.5)(x)
+    # block3
+    x = Conv2D(128, kernel_size=(3, 3), strides=1, padding='same', activation='relu')(x)
+    x = Conv2D(128, kernel_size=(3, 3), strides=1, padding='same', activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = Dropout(0.5)(x)
+    # fc
+    x = Flatten()(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(128, activation='relu')(x)
+    output_layer = Dense(n_classes, activation='softmax')(x)
+
+    model = Model(inputs=input_layer, outputs=output_layer)
+    return model
+
+
+def CNN2(input_shape=(48, 48, 1), n_classes=8):
+    """
+    参考论文Going deeper with convolutions在输入层后加一层的1*1卷积增加非线性表示
+    :param input_shape:
+    :param n_classes:
+    :return:
+    """
+    # input
+    input_layer = Input(shape=input_shape)
+    # block1
+    x = Conv2D(32, (1, 1), strides=1, padding='same', activation='relu')(input_layer)
+    x = Conv2D(32, (5, 5), strides=1, padding='same', activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # block2
+    x = Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # block3
+    x = Conv2D(64, (5, 5), padding='same', activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # fc
+    x = Flatten()(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(n_classes, activation='softmax')(x)
+
+    model = Model(inputs=input_layer, outputs=x)
+    return model
